a30d0da7a912aa323496f877587bc33ca9388312
Peppi78 <markus.peppler@googlemail.com>
2017-10-28 09:27:31 +0200

    Initialer Clone

--- MCCManegeFrei/Pages/About.cshtml.cs
+++ MCCManegeFrei/Pages/About.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MCCManegeFrei.Pages
+namespace ManegeFreiRazor.Pages
 {
     public class AboutModel : PageModel
     {
